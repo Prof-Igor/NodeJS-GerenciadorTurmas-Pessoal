d4d623067a53734882600812a43eafc7d1aa7e99
Prof-Igor <igorms.edu@gmail.com>
2023-11-01 18:01:53 -0300

    Configurnado planetscale

--- database/index.js
+++ database/index.js
@@ -7,10 +7,15 @@ const pool = mysql.createPool({
     user: process.env.DB_USERNAME, 
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME,
+    ssl:{
+        rejectUnauthorized: true
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
+
+//const pool = mysql.createPool(process.env.DATABASE_URL)
 
 pool.getConnection((err, conn) => {
     if(err) console.log(err)
